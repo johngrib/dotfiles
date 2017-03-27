b0e2a42f461dedf0538d534ca86772e3d5c2df46
John Grib <johngrib@woowahan.com>
2017-03-27 16:58:42 +0900

    [hammerspoon] hintChars 설정

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,10 +3,11 @@
 require('modules.caffein')
 require('modules.mouse')
 local caffein = require('modules.caffein'):init({'shift'}, 'f15')
-local vim_mode = require('modules.vim'):init('f16', 'f19')
+local vim_mode = require('modules.vim'):init('f16', 'f20')
 local app_manager = require('modules.appman'):init('f18')
 local mouse = require('modules.mouse'):init('f17')
 
 hs.alert.show('loaded')
 hs.hotkey.bind({'cmd'}, 'F17', hs.hints.windowHints)
+hs.hints.hintChars = {'F', 'U', 'I', 'O', 'P', 'H', 'J', 'K', 'L', 'B', 'N', 'M', 'Q', 'W', 'E', 'R', 'Z', 'X', 'C', 'V' }
 
