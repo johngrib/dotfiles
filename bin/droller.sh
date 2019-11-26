08319efdb773e798867ef86b22e00764003c913d
John Grib <johngrib82@gmail.com>
2019-11-26 23:43:13 +0900

    도움말 구분자를 - 에서 # 으로 변경하라

--- bin/droller.sh
+++ bin/droller.sh
@@ -7,19 +7,19 @@ function droller() {
     cmd="$1"
     uri="$2"
 
-    # status, s - 상태를 확인한다
+    # status, s # 상태를 확인한다
     if [ "$cmd" == "status" -o "$cmd" == "s" ]; then
         head -1 $tmp_file | cut -c 42-
         return 0;
     fi
 
-    # open, o - 현재 지정된 문서를 열어준다
+    # open, o # 현재 지정된 문서를 열어준다
     if [ "$cmd" == "open" -o "$cmd" == "o" ]; then
         head -1 $tmp_file | cut -d' ' -f4 | xargs open
         return 0;
     fi
 
-    # 1 - 현재 지정된 문서에 +1
+    # 1 # 현재 지정된 문서에 +1
     if [ "$cmd" == "1" ]; then
         hash=`head -1 $tmp_file | cut -d' ' -f1`
         point=`head -1 $tmp_file | awk '{print $2}'`
@@ -32,21 +32,21 @@ function droller() {
         return 0;
     fi
 
-    # top, t - 인덱스의 가장 위에 있는 문서를 선택한다
+    # top, t # 인덱스의 가장 위에 있는 문서를 선택한다
     if [ "$cmd" == "top" -o "$cmd" == "t" ]; then
         head -1 $index > $tmp_file
         droller s
         return 0;
     fi
 
-    # , random, r - 랜덤으로 문서를 선택한다.
+    # , random, r # 랜덤으로 문서를 선택한다.
     if [ "$cmd" == "" -o "$cmd" == "random" -o "$cmd" == "r" ]; then
         sort -R $index | head -1 | awk "{ print $1 }" > $tmp_file
         droller s
         return 0;
     fi
 
-    # edit - 선택된 문서 정보를 편집한다
+    # edit # 선택된 문서 정보를 편집한다
     if [ "$cmd" == "edit" -o "$cmd" == "e" ]; then
         vim $tmp_file
         if [ "$?" == "0" ]; then
@@ -58,13 +58,13 @@ function droller() {
         return 0;
     fi
 
-    # hash - sha1 값을 계산한다
+    # hash # sha1 값을 계산한다
     if [ "$cmd" == "hash" ]; then
         openssl sha1 <<< $uri
         return 0
     fi
 
-    # add, d - 새로운 문서를 추가한다
+    # add, d # 새로운 문서를 추가한다
     if [ "$cmd" == "add" -o "$cmd" == "a" ]; then
         hash=`openssl sha1 <<< $uri`
 
@@ -81,12 +81,12 @@ function droller() {
 
     fi
 
-    # help - 도움말을 출력한다
+    # help # 도움말을 출력한다
     if [ "$cmd" == "help" -o "$cmd" == "h" ]; then
-        egrep "^\s*?# " $BASH_SOURCE | sed 's/^ *#//' | column -ts'-'
+        egrep "^\s*?# " $BASH_SOURCE | sed 's/^ *#//' | column -ts'#'
     fi
 
-    # http:// - 새로운 문서를 추가한다
+    # http:// # 새로운 문서를 추가한다
     egrep '^https?:' <<< $1 > /dev/null
     if [ "$?" == "0" ]; then
         uri="$1"
