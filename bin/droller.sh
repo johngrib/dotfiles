9c4b1c5feafd1da5b0dd6c202e68d7ee75c19576
John Grib <johngrib82@gmail.com>
2019-11-26 23:10:53 +0900

    도움말을 볼 수 있게 하라

--- bin/droller.sh
+++ bin/droller.sh
@@ -7,33 +7,33 @@ function droller() {
     cmd="$1"
     uri="$2"
 
-    # status, s : 상태를 확인한다
+    # status, s - 상태를 확인한다
     if [ "$cmd" == "status" -o "$cmd" == "s" ]; then
         head -1 $tmp_file | cut -c 42-
         return 0;
     fi
 
-    # open, o : 현재 지정된 문서를 열어준다
+    # open, o - 현재 지정된 문서를 열어준다
     if [ "$cmd" == "open" -o "$cmd" == "o" ]; then
         head -1 $tmp_file | cut -d' ' -f4 | xargs open
         return 0;
     fi
 
-    # top, t : 인덱스의 가장 위에 있는 문서를 선택한다
+    # top, t - 인덱스의 가장 위에 있는 문서를 선택한다
     if [ "$cmd" == "top" -o "$cmd" == "t" ]; then
         head -1 $index > $tmp_file
         droller s
         return 0;
     fi
 
-    # "", random, r : 랜덤으로 문서를 선택한다.
+    # , random, r - 랜덤으로 문서를 선택한다.
     if [ "$cmd" == "" -o "$cmd" == "random" -o "$cmd" == "r" ]; then
         sort -R $index | head -1 | awk "{ print $1 }" > $tmp_file
         droller s
         return 0;
     fi
 
-    # edit : 선택된 문서 정보를 편집한다
+    # edit - 선택된 문서 정보를 편집한다
     if [ "$cmd" == "edit" -o "$cmd" == "e" ]; then
         vim $tmp_file
         if [ "$?" == "0" ]; then
@@ -45,13 +45,13 @@ function droller() {
         return 0;
     fi
 
-    # hash : sha1 값을 계산한다
+    # hash - sha1 값을 계산한다
     if [ "$cmd" == "hash" ]; then
         openssl sha1 <<< $uri
         return 0
     fi
 
-    # add, d : 새로운 문서를 추가한다
+    # add, d - 새로운 문서를 추가한다
     if [ "$cmd" == "add" -o "$cmd" == "a" ]; then
         hash=`openssl sha1 <<< $uri`
 
@@ -68,7 +68,12 @@ function droller() {
 
     fi
 
-    # droller http:// : 새로운 문서를 추가한다
+    # help - 도움말을 출력한다
+    if [ "$cmd" == "help" -o "$cmd" == "h" ]; then
+        egrep "^\s*?# " $BASH_SOURCE | sed 's/^ *#//' | column -ts'-'
+    fi
+
+    # http:// - 새로운 문서를 추가한다
     egrep '^https?:' <<< $1 > /dev/null
     if [ "$?" == "0" ]; then
         uri="$1"
