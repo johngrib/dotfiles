faacd453b3db54608ffa98deb34f21d889705d69
JohnGrib <johngrib82@gmail.com>
2024-02-03 15:45:11 +0900

    불필요한 기호 제거

--- bash-include/prompt.sh
+++ bash-include/prompt.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # colors
-GREEN='\e[0;32m\]'
-B_GREEN='\e[1;32m\]'
-MAGENTA='\e[0;35m\]'
-B_MAGENTA='\e[1;35m\]'
-YELLOW='\e[0;33m\]'
-B_YELLOW='\e[1;33m\]'
+GREEN='\e[0;32m'
+B_GREEN='\e[1;32m'
+MAGENTA='\e[0;35m'
+B_MAGENTA='\e[1;35m'
+YELLOW='\e[0;33m'
+B_YELLOW='\e[1;33m'
 RED='\e[0;31m'
 BLUE='\e[0;34m'
 B_BLUE='\e[1;34m'
-CYAN='\e[0;36m\]'
-COLOR_END='\[\033[0m\]'
+CYAN='\e[0;36m'
+COLOR_END='\e[0m'
 
 # PS1="\h:\W \u\$ "  # default promopt
 function gbr {
