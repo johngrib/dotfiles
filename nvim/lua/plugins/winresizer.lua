66a308042ed043f82569790536bfee1cc3c8e711
JohnGrib <johngrib82@gmail.com>
2025-08-17 20:57:14 +0900

    winresizer 설정 마이그레이션: lua/plugins/winresizer.lua 추가

---
+++ nvim/lua/plugins/winresizer.lua
@@ -0,0 +1,8 @@
+return {
+  'simeji/winresizer',
+  init = function()
+    vim.g.winresizer_start_key = '<f4>'
+    vim.g.winresizer_gui_start_key = '<f4>'
+    vim.g.winresizer_keycode_cancel = 122
+  end,
+}
