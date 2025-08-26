309f75d6e088a74e4b92d47a8a990ce583b1d4b0
JohnGrib <johngrib82@gmail.com>
2025-08-26 19:52:51 +0900

    wezterm color 지정

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,97 +1,39 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+local config = {}
 
--- https://wezfurlong.org/wezterm/config/files.html
-local config = {
-    -- font = wezterm.font 'JetBrains Mono',
-    font = wezterm.font 'MesloLGMDZ Nerd Font Mono',
-    font_size = 18,
-    visual_bell = {
-        fade_in_function = 'EaseIn',
-        fade_in_duration_ms = 100,
-        fade_out_function = 'EaseOut',
-        fade_out_duration_ms = 100,
+config.font = wezterm.font 'MesloLGMDZ Nerd Font Mono'
+config.tab_bar_at_bottom = true
+
+config.initial_cols = 120
+config.initial_rows = 28
+config.font_size = 23
+
+-- color: https://wezterm.org/config/appearance.html
+config.colors = {
+    foreground = 'f3f2f9',
+    background = '1b1a39',
+    ansi = {
+        '#000000',  -- normal black
+        '#d9513f',  -- normal red
+        '#7fc54f',  -- normal green
+        '#cf6630',  -- normal yellow
+        '#5b6fec',  -- normal blue
+        '#8075ab',  -- normal magenta
+        '#6c9486',  -- normal cyan
+        '#eed7b4',  -- normal white
     },
-    colors = {
-        visual_bell = '#202020',
-        foreground = '#f4f2fa',
-        background = '#13122a',
-        cursor_bg = '#cc5151',
-        cursor_fg = '#ffffff',
-        cursor_border = '#52ad70',
-        selection_fg = '#4d50ec',
-        selection_bg = '#c8d3e0',
-        scrollbar_thumb = '#222222',
-        -- The color of the split lines between panes
-        split = '#444444',
-        ansi = {
-            '#000000', -- black
-            '#ec4434', -- red
-            '#67c837', -- green
-            '#df5f19', -- yellow
-            '#5670f5', -- blue
-            '#8375b0', -- magenta
-            '#609686', -- cyan
-            '#f3d6b0', -- white
-        },
-        brights = {
-            '#080808', -- bright black
-            '#f97a70', -- bright red
-            '#8fff5b', -- bright green
-            '#fcce2e', -- bright yellow
-            '#48b3e5', -- bright blue
-            '#c07c74', -- bright magenta
-            '#bdfaf0', -- bright cyan
-            '#f4f2fa', -- bright white
-        },
-        tab_bar = {
-            active_tab = {
-                fg_color = '#ffd700',  -- gold color
-                bg_color = '#000000',
-            },
-        },
+    brights = {
+        '#080808',  -- bright black
+        '#e88174',  -- bright red
+        '#aafc73',  -- bright green
+        '#f4cf52',  -- bright yellow
+        '#66b0e0',  -- bright blue
+        '#b67f76',  -- bright magenta
+        '#c9f8ef',  -- bright cyan
+        '#f3f2f9',  -- bright white
     },
-    audible_bell = "Disabled",
-    window_frame = {
-        -- The font used in the tab bar.
-        -- Roboto Bold is the default; this font is bundled
-        -- with wezterm.
-        -- Whatever font is selected here, it will have the
-        -- main font setting appended to it to pick up any
-        -- fallback fonts you may have used there.
-        font = wezterm.font { family = 'MesloLGSDZ Nerd Font Mono', weight = 'Bold' },
-
-        -- The size of the font in the tab bar.
-        -- Default to 10. on Windows but 12.0 on other systems
-        font_size = 13.0,
-
-        -- The overall background color of the tab bar when
-        -- the window is focused
-        active_titlebar_bg = '#13122a',
-
-        -- The overall background color of the tab bar when
-        -- the window is not focused
-        -- inactive_titlebar_bg = '#333333',
-    },
-    send_composed_key_when_left_alt_is_pressed = false,
-    use_dead_keys = false,
-    use_ime = true,
-    enable_scroll_bar = true,
 }
-
-config.keys = {
-    -- command + m 을 끈다.
-    { key = 'm', mods = 'CMD', action = wezterm.action.DisableDefaultAssignment, },
-}
-
--- https://wezfurlong.org/wezterm/config/lua/config/window_padding.html
-config.window_padding = {
-    left = '3px',
-    right = '3px',
-    top = '0px',
-    bottom = '5px',
-}
-
-config.use_fancy_tab_bar = false
 
 return config
