29184b34fb282d485f497632493f7c38720381da
JohnGrib <johngrib82@gmail.com>
2025-08-27 20:29:15 +0900

    wezterm: 이것저것 업데이트

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,8 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
 local config = {}
 
 config.font = wezterm.font 'MesloLGMDZ Nerd Font Mono'
-config.tab_bar_at_bottom = true
 
 config.initial_cols = 120
 config.initial_rows = 28
@@ -38,7 +36,8 @@ config.colors = {
 
 local act = wezterm.action
 config.keys = {
-    { key = 'm', mods = 'CMD', action = act.ActivateCommandPalette, },
+    { key = 'm', mods = 'CMD', action = wezterm.action.ShowTabNavigator },
+    { key = 'h', mods = 'CMD', action = act.ActivateCommandPalette, },
     { key = 'd', mods = 'CMD', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     { key = 'd', mods = 'CMD|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
     { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = true } },
@@ -50,5 +49,53 @@ config.keys = {
 --     { regex = "JIRA-(\\d+)", format = "https://jira.example.com/browse/JIRA-$1" },
 --     { regex = "ISSUE-(\\d+)", format = "https://github.com/yourorg/yourrepo/issues/$1" },
 -- }
+
+-- tab bar 설정
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+config.window_frame = {
+    -- tab bar 폰트
+    font_size = 20.0,
+    font = wezterm.font { family = 'MelsoLGSDZ Nerd Font Mono', weight = 'Bold' },
+}
+
+-- Link: https://github.com/sapegin/dotfiles/blob/359cd798a0a30a2cdb1aabfff8ae9b265001e164/tilde/.wezterm.lua#L215
+local function get_current_working_dir(tab)
+    local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = '' }
+    local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
+
+    return current_dir == HOME_DIR and '.'
+    or string.gsub(current_dir.file_path, '(.*[/\\])(.*)', '%2')
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local index = tonumber(tab.tab_index) + 1
+    local title = get_current_working_dir(tab)
+    return string.format('%s:%s', index, title)
+end)
+
+config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
+
+-- mouse
+config.mouse_bindings = {
+    -- 마우스 드래그로 선택하면 클립보드로 복사
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'NONE',
+        action = wezterm.action.CompleteSelection('ClipboardAndPrimarySelection'),
+    },
+    -- command + click 으로 link 열기
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'CMD',
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+}
+
+-- Command Palette
+-- config.command_palette_rows = 40
+config.command_palette_font_size = 15
+config.command_palette_bg_color = "#44382D"
+config.command_palette_fg_color = "#c4a389"
 
 return config
