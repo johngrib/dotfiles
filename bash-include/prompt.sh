492787cbe1c6da9f6fce2421fd93d68f701b0e29
JohnGrib <johngrib82@gmail.com>
2026-03-15 08:56:02 +0900

    bash 프롬프트 업데이트

--- bash-include/prompt.sh
+++ bash-include/prompt.sh
@@ -11,13 +11,16 @@ RED='\e[0;31m'
 BLUE='\e[0;34m'
 B_BLUE='\e[1;34m'
 CYAN='\e[0;36m'
+B_RED='\e[1;31m'
+B_CYAN='\e[1;36m'
+B_GREEN_LIGHT='\e[0;92m'
 COLOR_END='\e[0m'
 
 # PS1="\h:\W \u\$ "  # default promopt
 function gbr {
     if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
         branch="$(git branch --show-current)"
-        branch_str="\033[1;031m$branch\033[0m"
+        branch_str="${B_RED}$branch${COLOR_END}"
 
         stat=$(git status --short 2> /dev/null \
             | cut -c 1-3 \
@@ -25,9 +28,10 @@ function gbr {
             | tr -d ' ' \
             | xargs)
 
+        hash_str="${CYAN}$(git rev-parse --short HEAD 2>/dev/null)${COLOR_END}"
         stash_size=$(git stash list 2> /dev/null | wc -l | sed 's/ //g')
-        stash_icon=" \e[0;92m≡\033[0m"
-        printf "[$branch_str] $stat$stash_icon$stash_size"
+        stash_icon=" ${B_GREEN_LIGHT}≡${COLOR_END}"
+        printf "[$hash_str][$branch_str] $stat$stash_icon$stash_size"
         return 0
     fi
 }
@@ -39,5 +43,3 @@ ${B_MAGENTA}\h \
 ${B_BLUE}\w \
 ${COLOR_END}\
 \$(gbr)\n\$ "
-
-
